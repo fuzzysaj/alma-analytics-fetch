9f3568a4ca25bf03938d75a3b494fac66894a2d3
Mark McCann <mamccan1@asu.edu>
2019-10-01 15:15:46 -0700

    version 0.0.5

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { AlmaApiError } from './AlmaApiError';
 export { getAlmaTable } from './aa-fetch-table';
-export { getColPositions, convertStr, rawJsonToClean } from './table-helpers';
+export { getColPositions, convertStr, rawJsonToClean,
+  ConvertType, DataType, ColMap } from './table-helpers';
 export { AATable, AARow, AAColumn } from './AATable';
